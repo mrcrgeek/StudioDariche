67af7f80b278f88ed7abc0061152d441d05bc917
mrcr_geek <mrcrsogand@gmail.com>
2022-03-12 22:45:17 +0330

    Add Art Delete Method

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::prefix('admin')->group(function (){
     Route::post('/login',[AdminController::class, 'login']);
     Route::post('/add_art', [WorkController::class, 'add_art']);
     Route::patch('/update_art/{id}', [WorkController::class, 'update_art'])->middleware('auth:admins');
+    Route::delete('/delete_art/{id}', [WorkController::class, 'delete_art'])->middleware('auth:admins');
     Route::get('/check_admin',[AdminController::class, 'check_admin'])->middleware('auth:admins');
 });
 
