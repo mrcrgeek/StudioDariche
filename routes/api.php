76abcad8495110b36c66bb89f6e6299f4ca5d058
mrcr_geek <mrcrsogand@gmail.com>
2022-03-12 12:17:54 +0330

    Add Update Art Route

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::prefix('admin')->group(function (){
     Route::post('/register',[AdminController::class, 'register']);
     Route::post('/login',[AdminController::class, 'login']);
     Route::post('/add_art', [WorkController::class, 'add_art']);
+    Route::patch('/update_art/{id}', [WorkController::class, 'update_art'])->middleware('auth:admins');
     Route::get('/check_admin',[AdminController::class, 'check_admin'])->middleware('auth:admins');
 });
 
