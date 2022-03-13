d32d4665ae069e0151a7a2bd2d119adca1d647f7
mrcr_geek <mrcrsogand@gmail.com>
2022-03-13 12:12:37 +0330

    Add Index Route

--- routes/api.php
+++ routes/api.php
@@ -16,13 +16,18 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::prefix('admin')->group(function (){
+Route::prefix('/admin')->group(function (){
+    //Auth
     Route::post('/register',[AdminController::class, 'register']);
     Route::post('/login',[AdminController::class, 'login']);
+    //works(slides)
     Route::post('/add_art', [WorkController::class, 'add_art']);
     Route::patch('/update_art/{id}', [WorkController::class, 'update_art'])->middleware('auth:admins');
     Route::delete('/delete_art/{id}', [WorkController::class, 'delete_art'])->middleware('auth:admins');
+    //middleware test
     Route::get('/check_admin',[AdminController::class, 'check_admin'])->middleware('auth:admins');
 });
+
+Route::get('/index', [WorkController::class, 'get_art']);
 
 
